c155e30b6b2e8b6b2302ffa2751e41999c317cc9
Swapan Pramanick <swapan.pramanick@zalando.de>
2019-07-10 11:29:59 +0200

    Initial commit

---
+++ src/test/java/com/swpraman/useraddress/UserAddressApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swpraman.useraddress;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserAddressApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
